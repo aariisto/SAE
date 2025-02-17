8b6c76b6132e04ac4abcf341d47129db07564920
aariisto <yannelaissani@gmail.com>
2025-02-17 01:12:46 +0100

    commentaire 100%

--- html/controller/PostGetController.php
+++ html/controller/PostGetController.php
@@ -1,32 +1,55 @@
 <?php
 
-require '/var/www/private/Model/Model.php';
-require '/var/www/private/functions/tokenCHECK.php'; 
+/**
+ * Cette fonction gère les requêtes POST envoyées au contrôleur.
+ * Elle vérifie que la requête est valide en s'assurant que :
+ * - La méthode HTTP est POST
+ * - Les en-têtes nécessaires (methode, csrf-token) sont présents
+ * - La session utilisateur est active et valide
+ * - Le jeton CSRF est vérifié
+ * 
+ * Si toutes les conditions sont remplies, elle inclut le fichier API correspondant à la méthode demandée.
+ * Sinon, elle renvoie une réponse d'erreur appropriée.
+ */
 
+// Inclusion des fichiers nécessaires
+require '/var/www/private/Model/Model.php'; // Classe Model
+require '/var/www/private/functions/tokenCHECK.php'; // Fonction de vérification du jeton CSRF
+
+// Démarre la session
 session_start();
 
+// Définit le type de contenu de la réponse en JSON
 header('Content-Type: application/json');
-$headers = getallheaders();
 
-$input = file_get_contents("php://input"); // Récupère les données envoyées
-$data = json_decode($input, true); // Décode les données JSON
+// Récupère tous les en-têtes de la requête
+$headers = getallheaders(); // csrf-token, methode
 
-// Vérifie que la méthode et les paramètres sont corrects
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($headers['methode']) 
+// Récupère les données envoyées dans le corps de la requête
+$input = file_get_contents("php://input");
+
+// Décode les données JSON reçues
+$data = json_decode($input, true);
+
+// Vérifie que la méthode est POST, que les en-têtes et les sessions sont valides, et que le jeton CSRF est vérifié
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($headers['methode']) && isset($headers['csrf-token']) 
         && isset($_SESSION['id']) && isset($_SESSION['token']) && verifyJWT($headers['csrf-token'],$_SESSION['id'])) {
 
-    $allowed_methods = ["post_search", "post_order","get_search","remove_search","get_reservation"]; // Liste des méthodes autorisées
+    // Liste des méthodes autorisées
+    $allowed_methods = ["post_search", "post_order","get_search","remove_search","get_reservation"]; // fichier API
 
     // Vérifie si la méthode fournie est dans la liste des méthodes autorisées
     if (in_array($headers['methode'], $allowed_methods)) {
+        // Inclut le fichier correspondant à la méthode
         require_once '/var/www/private/api_files/' . $headers['methode'] . '.php';
         
     } else {
+        // Réponse d'erreur si la méthode n'est pas autorisée
         echo json_encode(["error" => "Invalid method"]);
     }
 } else {
-    // Réponse d'erreur si l'action est invalide
-    http_response_code(401); // Erreur 400 : Mauvaise requête
+    // Réponse d'erreur si la requête est invalide ou si la session a expiré
+    http_response_code(401); // Erreur 401 : Non autorisé
     echo json_encode(["error" => "Votre session a expiré. Veuillez vous reconnecter.","error_code"=> "erreur code: 1","token"=> true]);
     exit();
 }
--- private/app.py
+++ private/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 import requests
 import re
-import time
 app = Flask(__name__)
 CORS(app)
 
@@ -10,14 +9,18 @@ CORS(app)
 
 @app.route('/stations', methods=['GET'])
 def get_stations():
+    # Effectuer une requête GET pour obtenir les informations des stations
     response = requests.get("https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json")
+    
+    # Vérifier si la requête a réussi
     if response.status_code == 200:
+        # Convertir la réponse JSON en dictionnaire Python
         data = response.json()
         formatted_data = []
 
-        # Filtrage et structuration des données
+        # Filtrer et structurer les données des stations
         for station in data.get('data', {}).get('stations', []):
-            # Validation des champs et types requis
+            # Valider les champs et types requis pour chaque station
             if (
                 isinstance(station.get('station_id'), int) and
                 isinstance(station.get('stationCode'), str) and
@@ -37,71 +40,95 @@ def get_stations():
                 }
                 formatted_data.append(formatted_station)
 
+        # Retourner les données formatées en JSON
         return jsonify(formatted_data)
     else:
+        # Retourner une erreur si la requête a échoué
         return jsonify({"error": f"Erreur lors de la requête: {response.status_code}"}), response.status_code
 
 
 @app.route('/station/<int:station_id>', methods=['GET'])
 def get_station_info(station_id):
-    
-    # Récupérer toutes les stations pour trouver celle qui correspond à station_id
+    # Effectuer une requête GET pour obtenir les informations de statut des stations
     response = requests.get("https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json")
+    
+    # Vérifier si la requête a réussi
     if response.status_code == 200:
+        # Convertir la réponse JSON en dictionnaire Python
         data = response.json()
+        
         # Trouver la station avec le station_id donné
         station_info = next((station for station in data['data']['stations'] if station['station_id'] == station_id), None)
         
+        # Si la station est trouvée, retourner ses informations
         if station_info:
             return jsonify(station_info)
         else:
+            # Retourner une réponse vide si la station n'est pas trouvée
             return jsonify(None)
     else:
+        # Retourner une erreur si la requête a échoué
         return jsonify({"error": f"Erreur lors de la requête: {response.status_code}"}), response.status_code
 
 
 @app.route('/search_station/<string:name>', methods=['GET'])
 def search_station(name):
-
+    # Effectuer une requête GET pour obtenir les informations des stations
     response = requests.get("https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json")
+    
+    # Vérifier si la requête a réussi
     if response.status_code == 200:
+        # Convertir la réponse JSON en dictionnaire Python
         stations_data = response.json()
+        
+        # Parcourir les stations pour trouver une correspondance avec le nom donné
         for station in stations_data['data']['stations']:
-          if (re.sub(r'[\s_-]+', ' ', name).strip().lower() == re.sub(r'[\s_-]+', ' ', station['name']).strip().lower()):
+            # Comparer les noms des stations en ignorant les espaces, tirets et majuscules
+            if (re.sub(r'[\s_-]+', ' ', name).strip().lower() == re.sub(r'[\s_-]+', ' ', station['name']).strip().lower()):
+                # Retourner les informations de la station trouvée
                 return jsonify({
                     "station_id": station['station_id'],
                     "lat": station['lat'],
                     "lon": station['lon']
                 })
-          
-  
-   # nameGoogle = name.replace(' ', '+')
-   # api_key = "AIzaSyDUm17xB5A406D0Z81JeCEaPT7HiWK1Qe4"
-    # URL de la requête
-   # url = f'https://maps.googleapis.com/maps/api/geocode/json?address={nameGoogle}&key={api_key}'
-    
-    # Faire la requête GET
-  #  response = requests.get(url)
-   # data = response.json()
-    
-    # Vérifier si la réponse contient des résultats
-   # if data['status'] == 'OK':
-   #     # Extraire la latitude et la longitude
-   #     lat = data['results'][0]['geometry']['location']['lat']
-   #     lng = data['results'][0]['geometry']['location']['lng']
-   #     return jsonify({
-   # #                "station_id": None ,
-      #              "lat": lat,
-    #                "lon": lng
-   #             })
-  
-        
-        return jsonify({"error": "Station non trouvée"})
     else:
+        # Retourner une erreur si la requête a échoué
         return jsonify({"error": "Erreur lors de la requête"}), response.status_code
     
+    """
+    # Préparer le nom pour la requête à l'API Google
+    name_google = name.replace(' ', '+')
+    api_key = "AIzaSyDUm17xB5A406D0Z81JeCEaPT7HiWK1Qe4"
+    url = f'https://maps.googleapis.com/maps/api/geocode/json?address={name_google}&key={api_key}'
+
+    # Effectuer une requête GET à l'API Google pour obtenir les coordonnées
+    response = requests.get(url)
+    
+    # Vérifier si la requête a réussi
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Vérifier si la réponse de l'API Google est valide
+        if data['status'] == 'OK':
+            lat = data['results'][0]['geometry']['location']['lat']
+            lng = data['results'][0]['geometry']['location']['lng']
+            
+            # Retourner les coordonnées obtenues de l'API Google
+            return jsonify({
+                "station_id": None,
+                "lat": lat,
+                "lon": lng
+            })
+    else:
+        # Retourner une erreur si la requête a échoué
+        return jsonify({"error": "Erreur lors de la requête"}), response.status_code
+    """
+    
+    # Retourner une erreur si aucune station n'est trouvée
+    return jsonify({"error": "Station non trouvée"})
+    
+    
     
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
